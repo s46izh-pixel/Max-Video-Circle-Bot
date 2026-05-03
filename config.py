032a60436843f0a84a5c05397b3ebf2368777d5e
s46izh-pixel <s46.izh@gmail.com>
2026-05-04 00:57:35 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 # Токен берется из переменных окружения (для Docker) или из строки ниже (для локального запуска)
-TOKEN = os.getenv("TOKEN", "YOUR_TOKEN")
+TOKEN = f9LHodD0cOJkcY3SZeb81wXnqvjM3srWayXgW-j0QgVuMhDWDjaA69xvXJfsYc7TNVcKVY_Qm804LYs8lcST("TOKEN", "YOUR_TOKEN")
 # URL для подключения к API MAX
-API_URL = os.getenv("API_URL", "https://max.pager.dev")
+API_URL = f9LHodD0cOJkcY3SZeb81wXnqvjM3srWayXgW-j0QgVuMhDWDjaA69xvXJfsYc7TNVcKVY_Qm804LYs8lcST("API_URL", "https://max.pager.dev")
